605fb613f1241c6d984c6bee838a012e854be2e9
Gerasimov <gerasimmaks@mail.ru>
2023-06-06 22:44:40 +0300

    task 32 completion

--- Seminar_6/task_32.py
+++ Seminar_6/task_32.py
@@ -7,8 +7,14 @@ len_arr = int(input('введите длину списка _'))
 arr_rand = [random.randint(1, 10) for _ in range(len_arr)]
 first_num = int(input('введите минимальное значение'))
 second_num = int(input('введите максимальное значение'))
-
+print(arr_rand)
 index_list = []
-for i in len_arr:
-    if i > first_num and i < second_num:
-        f
+current_index = 0
+for i in arr_rand:
+    if (i >= first_num) and (i <= second_num):
+        index_list.append(current_index)
+    current_index += 1
+print(index_list)
+
+
+
