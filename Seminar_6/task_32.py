cbfdf75ad5c32b9ea8a441133e97496f00a2ef17
Gerasimov <gerasimmaks@mail.ru>
2023-06-06 21:28:17 +0300

    1

--- Seminar_6/task_32.py
+++ Seminar_6/task_32.py
@@ -1,3 +1,14 @@
  # Определить индексы элементов массива (списка),
  # значения которых принадлежат заданному диапазону
  # (т.е. не меньше заданного минимума и не больше заданного максимума)
+
+import random
+len_arr = int(input('введите длину списка _'))
+arr_rand = [random.randint(1, 10) for _ in range(len_arr)]
+first_num = int(input('введите минимальное значение'))
+second_num = int(input('введите максимальное значение'))
+
+index_list = []
+for i in len_arr:
+    if i > first_num and i < second_num:
+        f
