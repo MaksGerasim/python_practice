e74c65094d749b27ede77d4b979b14e68e0e8f9c
Gerasimov <gerasimmaks@mail.ru>
2023-06-13 23:56:31 +0300

    23:56

--- Seminar_8/task_38.py
+++ Seminar_8/task_38.py
@@ -8,8 +8,6 @@ def print_guide(file_csv):
     print(print_f.read())
     print_f.close()
 
-
-
 def create_new_rows():
     new_rows = []
     new_rows.append(input('введите фамилию_'))
@@ -17,18 +15,24 @@ def create_new_rows():
     new_rows.append(input('введите номер телефона_'))
     return (new_rows)
 
-
 def writ_rows_guide(file_csv, list_data):
     from csv import writer
-    with open(file_csv, 'a', newline='', encoding='utf-8') as f_object:
-        writer_object = writer(f_object)
+    with open(file_csv, 'a', newline='', encoding='utf-8') as n_file:
+        writer_object = writer(n_file)
         writer_object.writerow(list_data)
-        f_object.close()
+        n_file.close()
+
+def telephone_number_search(file_csv):
+    f_search = open(file_csv, 'r', encoding='utf-8')
+    file = csv.reader(f_search)
+    data_search = {input('введите фамилию_'), input('введите имя_')}
+    for x in file:
+        if (x[0] in data_search) and (x[1] in data_search):
+            print(f'номер телефона: {x[2]}')
 
 
 
-
-
-new_rows = create_new_rows()
-writ_rows_guide(file_csv='telephone_directory.csv', list_data=new_rows)
-print_guide(file_csv='telephone_directory.csv',)
+#new_rows = create_new_rows()
+#writ_rows_guide(file_csv='telephone_directory.csv', list_data=new_rows)
+print_guide(file_csv='telephone_directory.csv')
+telephone_number_search(file_csv='telephone_directory.csv')
--- Seminar_8/temp.py
+++ Seminar_8/temp.py
@@ -1,14 +1,42 @@
 import csv
 
+f = open('telephone_directory.csv', 'r', encoding='utf-8')
+file = f.readlines()
+print(file)
+f.close()
+f = open('telephone_directory.csv', 'w', encoding='utf-8')
+data_search = {'Петров', 'Петр'}
+for x in file:
+    f.write(x)
+    print(x)
 
-# f = open('telephone_directory.csv', 'r', encoding='utf-8')
-# print(f.read())
-# f.close()
+
+f.close()
 
 
-f = open('telephone_directory.csv', 'r+', encoding='utf-8')
-new_list = []
-list = {'aaa'}
-for line in f:
+
+    # text_poem = input('введите текст_ ')
+    # result_text = text_poem.ljust(len(text_poem) + 1, ' ')
+    # guide_ru = {"а", "е", "ё", "и", "о", "у", "ы", "э", "ю", "я"}
+    # count_let = []
+    # count = 0
+    # for i in result_text:
+    #     if (i.isspace()) != True:
+    #         if i in guide_ru:
+    #             count += 1
+    #     else:
+    #         count_let.append(count)
+    #         count = 0
+    # print(count_let)
+    # sum_el = 0
+    # for el in count_let:
+    #     sum_el += el
+    # if sum_el / len(count_let) == el:
+    #     print('“Парам пам-пам”')
+    # else:
+    #     print('“Пам парам”')
 
 
+# Ивано, Иван, 67777777
+# Петров, Петр, 98888888
+# Семенов, Семен, 89999999
