8a24c8c1762ba33b2387d69cebb5c6db0127ff8a
Gerasimov <gerasimmaks@mail.ru>
2023-06-14 23:41:46 +0300

    ___

--- Seminar_9/task_40.py
+++ Seminar_9/task_40.py
@@ -1,2 +1,7 @@
 # Работать с файлом california_housing_train.csv, который находится в папке sample_data.
 # Определить среднюю стоимость дома, где кол-во людей от 0 до 500 (population).
+
+import pandas as pd
+
+data = pd.read_csv('california_housing_test.csv')
+print(type(data))
